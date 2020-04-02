2a80bf974f55ed01dfa9cdc126df40e9ace41055
Ally Brannon <allison.brannon@gmail.com>
2020-04-02 14:49:21 -0400

    add cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors(corsOptions));
 app.use("/", indexRouter);
 
 // app.engine("html", es6Renderer);
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get("/", async function(req, res, next) {
   res.json("Coronavirus Blog API").status(200);
 });
 
+router.get("/all", async function(req, res, next) {
+  let blogs = await blogModel.getallBlog();
+  res.json(blogs);
+});
+
 module.exports = router;
